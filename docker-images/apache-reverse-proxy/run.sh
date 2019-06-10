7bdbc93f2dc3bd44f69d6146ff3424a72c3e8637
Christoph Rueff <christoph.rueff@gmail.com>
2019-06-10 16:20:32 +0200

    Get docker IPs automatically to set-up reverse proxy in script, fix express_dynamic to install modules

--- docker-images/apache-reverse-proxy/run.sh
+++ docker-images/apache-reverse-proxy/run.sh
@@ -1,20 +1,29 @@
+# kill and remove previous dockers
+docker kill apache_static
+docker rm apache_static
+docker kill express_dynamic
+docker rm express_dynamic
+docker kill apache_rp
+docker rm apache_rp
+
 # Image with static content
 # No port mapping, runs in background
 # -d = mode daemon
 docker build -t res/apache_php ../apache-php-image/
-# This dummy container is launched just to show our configuration is working properly
-docker run -d res/apache_php
 docker run -d --name apache_static res/apache_php
 
 # Image with the API animals
 # No port mapping, runs in background
 docker build -t res/express_animals ../express-image/
-# This dummy container is launched just to show our configuration is working properly
-docker run -d res/express_animals
 docker run -d --name express_dynamic res/express_animals
 
+# Get the docker IDs
+static_app=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' apache_static)
+echo "static_app : $static_app"
+express_app=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' express_dynamic)
+echo "express_app : $express_app"
 
 # This will build and run the image of the reverse proxy
 # -e allows to add an env variable, you'd have to change this IPs to correspond to your configuration
 docker build -t res/apache_rp .
-docker run -e STATIC_APP=172.17.0.3:80 -e DYNAMIC_APP=172.17.0.5:3000 --name apache_rp -p 8080:80 res/apache_rp
+docker run -e STATIC_APP=$static_app:80 -e DYNAMIC_APP=$express_app:3000 --name apache_rp -p 8080:80 res/apache_rp
